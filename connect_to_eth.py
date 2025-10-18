f340661c28a88bb2b9d4bd35f83c987d409be4d1
salam002 <salam2@berkeley.edu>
2025-10-18 09:36:58 -0700

    eth script

---
+++ connect_to_eth.py
@@ -0,0 +1,47 @@
+import json
+from web3 import Web3
+from web3.middleware import ExtraDataToPOAMiddleware
+from web3.providers.rpc import HTTPProvider
+
+'''
+If you use one of the suggested infrastructure providers, the url will be of the form
+now_url  = f"https://eth.nownodes.io/{now_token}"
+alchemy_url = f"https://eth-mainnet.alchemyapi.io/v2/{alchemy_token}"
+infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
+'''
+
+def connect_to_eth():
+	url = "https://ethereum-mainnet.core.chainstack.com/2b35d5f7b6b9fe18521829110a7e61cd"  # FILL THIS IN
+	w3 = Web3(HTTPProvider(url))
+	assert w3.is_connected(), f"Failed to connect to provider at {url}"
+	return w3
+
+
+def connect_with_middleware(contract_json):
+	with open(contract_json, "r") as f:
+		d = json.load(f)
+		d = d['bsc']
+		address = d['address']
+		abi = d['abi']
+
+	# TODO complete this method
+	# url = "https://ethereum-mainnet.core.chainstack.com/2b35d5f7b6b9fe18521829110a7e61cd"  # FILL THIS IN
+	# w3 = Web3(HTTPProvider(url))
+	bnb_testnet_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+	w3 = Web3(HTTPProvider(bnb_testnet_url))
+    
+    assert w3.is_connected(), f"Failed to connect to BNB testnet at {bnb_testnet_url}"
+    
+	# The first section will be the same as "connect_to_eth()" but with a BNB url
+	w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+	# The second section requires you to inject middleware into your w3 object and
+	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
+	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
+	contract = w3.eth.contract(address=Web3.to_checksum_address(address), abi=abi)
+
+	return w3, contract
+
+
+if __name__ == "__main__":
+	connect_to_eth()
